c68b2f8ecb46193ba76152b4d585227730e14790
Remi <remi.fusade@gmail.com>
2015-03-25 22:43:10 +0100

    Ajout projet
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ UnityProject/Assets/Scripts/GameEngineBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEngineBehaviour : MonoBehaviour {
+
+	public Animator greenLightAnimator;
+	public Animator redLightAnimator;
+
+	public float greenLightTimer;
+	public float redLightTimer;
+
+	public bool greenLightIsOn;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		greenLightIsOn = Mathf.FloorToInt (Time.time) % (greenLightTimer + redLightTimer) < greenLightTimer;
+		greenLightAnimator.SetBool("greenLightOn", greenLightIsOn);
+		redLightAnimator.SetBool("greenLightOn", greenLightIsOn);
+	}
+}
---
+++ UnityProject/Assets/Scripts/RideauSideBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RideauSideBehaviour : MonoBehaviour {
+
+	public Camera currentCamera;
+
+	public bool isRight;
+
+	private float pixelsInsideRatio;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pixelsInsideRatio = 0.09f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (isRight)
+		{
+
+			// droite
+			this.transform.position = currentCamera.ScreenToWorldPoint(new Vector3(currentCamera.pixelWidth*(1-pixelsInsideRatio), currentCamera.pixelHeight/2, 25));
+		}
+		else
+		{
+			// gauche
+			this.transform.position = currentCamera.ScreenToWorldPoint(new Vector3(currentCamera.pixelWidth*pixelsInsideRatio, currentCamera.pixelHeight/2, 25));
+		}
+	}
+}
